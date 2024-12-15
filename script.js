852555eeb52fd54760efcd29835352e62478b735
Luke Vizor <LukeJRVizor@protonmail.com>
2024-12-15 09:13:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const body = document.body;
 const header = document.querySelector("header");
 
 // Function to [Toggle Dark Mode]
-function toggleDarkMode() {
+function toggleDarkMode() 
+{
   // Toggle Dark Mode Class [Body/Header]
   body.classList.toggle("dark-mode");
   header.classList.toggle("dark-mode");
+}
+
+toggleButton.addEventListener("click", toggleDarkMode);
+};
 
